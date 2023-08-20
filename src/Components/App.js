2e2851405d7e32a3446ec66b416cb4fc2a672cf2
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-08-20 14:21:28 -0400

    Implemented react router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,34 +1,71 @@
-import React, {useState} from 'react'
-import About from "./About";
-import Contact from "./Contact";
-import Header from './Header'
-import Skills from "./Skills";
+// import React, {useState} from 'react'
+// import About from "./About";
+// import Contact from "./Contact";
+// import Header from './Header'
+// import Skills from "./Skills";
+// import Nav from './Nav';
+// import Demos from './Demos';
+// import "./App.css"
+
+
+// function App( ) {
+//   const [selected, setSelected] = useState("About")
+
+//   const handleSelection= (newSelection) => {    
+//     setSelected(newSelection)
+//   }
+
+//   return (
+  
+//     <div className="App">
+        
+//       <Header/>
+//       <Nav selected={selected} handleSelection={handleSelection} />
+//         <main>
+//           {selected === "About" && <About />}
+//           {selected === "Skills" && <Skills />}
+//           {selected === "Contact" && <Contact />}
+//           {selected === "Demos" && <Demos />}
+//         </main>       
+//     </div>
+    
+//   );
+// }
+
+// export default App;
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import About from './About';
+import Contact from './Contact';
+import Header from './Header';
+import Skills from './Skills';
 import Nav from './Nav';
 import Demos from './Demos';
-import "./App.css"
+import './App.css';
 
+function App() {
+  const [selected, setSelected] = useState('About');
 
-function App( ) {
-  const [selected, setSelected] = useState("About")
-
-  const handleSelection= (newSelection) => {    
-    setSelected(newSelection)
-  }
+  const handleSelection = (newSelection) => {
+    setSelected(newSelection);
+  };
 
   return (
-  
-    <div className="App">
-       
-      <Header/>
-      <Nav selected={selected} handleSelection={handleSelection} />
+    <Router>
+      <div className="App">
+        <Header />
+        <Nav selected={selected} handleSelection={handleSelection} />
         <main>
-          {selected === "About" && <About />}
-          {selected === "Skills" && <Skills />}
-          {selected === "Contact" && <Contact />}
-          {selected === "Demos" && <Demos />}
-        </main>       
-    </div>
-    
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/demos" element={<Demos />} />
+            <Route path="/qr" element={<Navigate to="/" />} /> {/* Redirect to the homepage */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,25 +1,43 @@
-import React, {useState} from 'react'
-import Links from './Links'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Links from './Links';
 
-function Nav ({selected, handleSelection}) {
- const [isOpen, setIsOpen] =useState(false)
- const pages = ['About', 'Skills', 'Contact', 'Demos']
+function Nav({ selected, handleSelection }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const pages = ['About', 'Skills', 'Contact', 'Demos'];
 
   return (
     <div>
-      <Links/>      
-      <button className="hamburger" onClick={() => setIsOpen(!isOpen)} style={{color: isOpen ? "grey" : '#B267E6'}}><i className="fa-solid fa-bars"></i></button>
-        <nav style={{display: isOpen ? "block" : "none"}}>  
-            <ul>                
-              {pages.map((page) => (
-                <li key={page} onClick={() => handleSelection(page)}>
+      <Links />
+      <button
+        className="hamburger"
+        onClick={() => setIsOpen(!isOpen)}
+        style={{ color: isOpen ? 'grey' : '#B267E6' }}
+      >
+        <i className="fa-solid fa-bars"></i>
+      </button>
+      <nav style={{ display: isOpen ? 'block' : 'none' }}>
+        <ul>
+           {pages.map((page) => (
+            <li key={page} onClick={() => handleSelection(page)}>
+              {page === 'About' ? (
+                <Link to="/" className={selected === 'About' ? 'selected' : ''}>
                   {page}
-                </li>
-              ))} 
-            </ul>
-        </nav>
+                </Link>
+              ) : (
+                <Link
+                  to={`/${page.toLowerCase()}`}
+                  className={selected === page ? 'selected' : ''}
+                >
+                  {page}
+                </Link>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
