1bc2103bfccd77f63e9e15803979ec4a05acd8a0
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-08-29 22:42:14 -0400

    Added new demos

--- src/Components/Demos.js
+++ src/Components/Demos.js
@@ -39,7 +39,8 @@ class Demos extends Component {
     render() {
         return (
             <div className='segment'>
-                <h1>Demo</h1>
+                <h1>Demos</h1>
+
                 <div className='content-container'>
                     <div className='video-container'>
                         <video controls width="100%" height="auto">
@@ -59,6 +60,235 @@ class Demos extends Component {
                         </div>
                     </div>
                 </div>
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/beautyhackerdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Beauty Blog</h2>
+                            <p>Navbar, blog, interlinked blog posts, contact form, newsletter form</p>        
+                        </div>
+                    </div>
+                </div>
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/moviedemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Movie Search</h2>
+                            <p>Search movies</p>        
+                        </div>
+                    </div>
+                </div>
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/flashcarddemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>React Flashcards</h2>
+                            <p>Responsive flashcards on react terminology</p>        
+                        </div>
+                    </div>
+                </div>
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/videoplayerdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Youtube Copycat</h2>
+                            <p>Search youtube's API and play videos</p>        
+                        </div>
+                    </div>
+                </div>   
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/redditcopycatdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Reddit Copycat</h2>
+                            <p>Search reddit's API</p>        
+                        </div>
+                    </div>
+                </div>  
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/expensetrackerdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Expense Tracker</h2>
+                            <p>Track your expenses</p>        
+                        </div>
+                    </div>
+                </div>       
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/BoBurnhamdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Welcome to the Internet</h2>
+                            <p>A funky little site about the worldwide web</p>        
+                        </div>
+                    </div>
+                </div>    
+
+                 <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/videoplayerdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Instagram Copycat</h2>
+                            <p>Front-end instagram copycat</p>        
+                        </div>
+                    </div>
+                </div>       
+
+                  <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/recipeblogdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Recipe Blog</h2>
+                            <p>Responsive design for a recipe web application</p>        
+                        </div>
+                    </div>
+                </div>             
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/tiktokdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Clock / Dark Mode Demo</h2>
+                            <p>Responsive clock changes to dark mode</p>        
+                        </div>
+                    </div>
+                </div>
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/drawingdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Sketchpad</h2>
+                            <p>Draw with mouse and increase size of lines.</p>        
+                        </div>
+                    </div>
+                </div>
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/colorboxdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Color boxes</h2>
+                            <p>Create boxes of random colors as you wave your mouse across the gameboard.</p>        
+                        </div>
+                    </div>
+                </div>
+
+   
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/quotedemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Random Quote Generator</h2>
+                            <p>Search or generate random from an API of quotes</p>        
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/imagesearchdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Image Search</h2>
+                            <p>Search API of images</p>        
+                        </div>
+                    </div>
+                </div>           
+
+                <div className='content-container'>
+                    <div className='video-container'>
+                        <video controls width="100%" height="auto">
+                            <source src="images/basiccomponentdemo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    <div className='text-container'>
+                        <div className='demo-description'>
+                            <h2>Basic Components</h2>
+                            <p>Navbar, accordian dropdown, translator</p>        
+                        </div>
+                    </div>
+                </div>     
+               
+
             </div>
         );
     }
